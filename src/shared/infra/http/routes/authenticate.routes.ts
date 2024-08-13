9aa947a48fb0f4a2505ec8b2bb35feca5fb770ae
Robert Vitoriano <robertvitorianodev@gmail.com>
2024-08-13 19:41:20 +0900

    fix google login screen

--- src/shared/infra/http/routes/authenticate.routes.ts
+++ src/shared/infra/http/routes/authenticate.routes.ts
@@ -2,12 +2,28 @@ import { Router } from "express";
 import { AuthenticateUserController } from "../../../../modules/accounts/useCases/authenticateUser/AuthenticateUserController";
 import { LogOutUserController } from "../../../../modules/accounts/useCases/logoutUser/LogOutUserController";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
-import { PassportService } from "../../../../modules/accounts/useCases/LoginOAuthUseCase/PassportService";
-
+import passport from "passport";
+import { Strategy } from "passport-google-oauth20";
+import dotenv from "dotenv";
+dotenv.config();
 const authenticateRoutes = Router();
 
 const authenticateController = new AuthenticateUserController();
 const logoutUserController = new LogOutUserController();
+
+passport.use(
+  new Strategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+      scope: ["profile", "email"],
+    },
+    async function (accessToken, refreshToken, profile, cb) {
+      console.log("HELLO WORLD");
+    }
+  )
+);
 
 authenticateRoutes.post("/log-in", authenticateController.handle);
 authenticateRoutes.post(
@@ -16,25 +32,14 @@ authenticateRoutes.post(
   logoutUserController.handle
 );
 
-const passportService = new PassportService();
-
-function passportGoogleAuth() {
-  passportService.setup();
-  return passportService
-    .getInstance()
-    .authenticate("google", { scope: ["profile", "email"] });
-}
-function passportGoogleCallback() {
-  passportService.setup();
-  return passportService
-    .getInstance()
-    .authenticate("google", { failureRedirect: "/login" });
-}
-authenticateRoutes.get("/auth/google", passportGoogleAuth);
+authenticateRoutes.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
 
 authenticateRoutes.get(
   "/auth/google/callback",
-  passportGoogleCallback,
+  passport.authenticate("google", { failureRedirect: "/login" }),
   function (req, res) {
     //@ts-ignore
     const { token } = res.req.user;
