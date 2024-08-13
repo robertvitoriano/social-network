6c577a9e5848cebd017c348317e553831ae9c8d7
Robert Vitoriano <robertvitorianodev@gmail.com>
2024-08-13 13:45:23 +0900

    change tsconfig file

--- ormconfig.js
+++ ormconfig.js
@@ -1,22 +1,41 @@
+const path = require('path');
+
+const isProduction = process.env.ENVIRONMENT === 'prod';
+
+const User = isProduction
+  ? require(path.join(__dirname, 'dist/src/modules/accounts/infra/typeorm/entities/User')).User
+  : require(path.join(__dirname, 'src/modules/accounts/infra/typeorm/entities/User')).User;
+
+const Friendship = isProduction
+  ? require(path.join(__dirname, 'dist/src/modules/friendships/infra/typeorm/entities/Friendship')).Friendship
+  : require(path.join(__dirname, 'src/modules/friendships/infra/typeorm/entities/Friendship')).Friendship;
+
+const Notification = isProduction
+  ? require(path.join(__dirname, 'dist/src/modules/notifications/infra/typeorm/entities/Notification')).Notification
+  : require(path.join(__dirname, 'src/modules/notifications/infra/typeorm/entities/Notification')).Notification;
+
+const Message = isProduction
+  ? require(path.join(__dirname, 'dist/src/modules/chat/infra/typeorm/entities/Message')).Message
+  : require(path.join(__dirname, 'src/modules/chat/infra/typeorm/entities/Message')).Message;
+
+const NotificationType = isProduction
+  ? require(path.join(__dirname, 'dist/src/modules/notifications/infra/typeorm/entities/NotificationType')).NotificationType
+  : require(path.join(__dirname, 'src/modules/notifications/infra/typeorm/entities/NotificationType')).NotificationType;
 
 const ormConfig = {
-  type: "mysql",
-  host: "localhost",
-  port: 3306,
-  username: "root",
-  password: "123",
-  database: "social_network_db",
-  migrations: [
-    "./dist/src/shared/infra/typeorm/migrations/*.{js}",
-    "./src/shared/infra/typeorm/migrations/*.{ts,js}"
-  ],
-  entities: [
-    "./dist/src/modules/**/entities/*.{js}",
-    "./src/modules/**/entities/*.{ts,js}"
-  ],  
-  logging: false,
+  type: 'mysql',
+  host: process.env.MYSQLDB_HOST,
+  port: parseInt(process.env.MYSQLDB_LOCAL_PORT, 10),
+  username: process.env.MYSQLDB_USER,
+  password: process.env.MYSQLDB_ROOT_PASSWORD,
+  database: process.env.MYSQLDB_DATABASE,
+  migrations: isProduction
+    ? ['./dist/src/shared/infra/typeorm/migrations/*.{js}']
+    : ['./src/shared/infra/typeorm/migrations/*.{ts,js}'],
+  entities: [User, Friendship, NotificationType, Notification, Message],
+  logging: !isProduction,
   cli: {
-    migrationsDir: "./src/shared/infra/typeorm/migrations"
+    migrationsDir: './src/shared/infra/typeorm/migrations'
   }
 };
 
