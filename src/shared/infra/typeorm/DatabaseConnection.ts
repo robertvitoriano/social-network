3dd4caf66a4572427937933c29d28dde1788542b
Robert Vitoriano <robertvitorianodev@gmail.com>
2025-07-29 22:57:19 -0300

    update dabase config

--- src/shared/infra/typeorm/DatabaseConnection.ts
+++ src/shared/infra/typeorm/DatabaseConnection.ts
@@ -1,3 +1,4 @@
+import { password } from "ormconfig.example";
 import { env } from "src/config/env";
 import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
@@ -8,7 +9,10 @@ class DatabaseConnection {
     const defaultOptions = await getConnectionOptions();
     return createConnection(
       Object.assign(defaultOptions, {
+        database:env.MYSQLDB_DATABASE,
+        user:env.MYSQLDB_USER,
         host,
+        password:env.MYSQLDB_ROOT_PASSWORD,
       })
     );
   }
